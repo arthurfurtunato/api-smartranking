d0c19466b1d6aa9c8469c3b5c7dde60ed1ef259b
arthurfurtunato <arthurfurtunato@gmail.com>
2024-01-02 17:48:56 -0300

    feat: adding mongo config and jogadores schema

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { JogadoresModule } from './jogadores/jogadores.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConnectOptions } from 'mongoose';
+import 'dotenv/config';
 
 @Module({
-  imports: [JogadoresModule],
+  imports: [
+    MongooseModule.forRoot(
+      `mongodb+srv://admin:${process.env.MONGOOSE_PASSWORD}@cluster-smart-api-ranki.eqkcrm2.mongodb.net/`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      } as ConnectOptions,
+    ),
+    JogadoresModule,
+  ],
   controllers: [],
   providers: [],
 })
